4526da885114d621c3d95fbd85ac4a84607e0ac2
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-10-07 17:30:59 +0900

    smartphone bill culculator

---
+++ src/main/java/com/example/smartphone/options/NetSecuirty.java
@@ -0,0 +1,13 @@
+package com.example.smartphone.options;
+
+import com.example.smartphone.Option;
+
+/** あんしんネットセキュリティ */
+public class NetSecuirty extends Option {
+
+    @Override
+    public int getBillPerMonth() {
+        return 500;
+    }
+
+}
